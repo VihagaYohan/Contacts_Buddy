20c33af9bef98427444d31bc750c71c811529c01
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-12-27 14:56:36 +0530

    Updated - UIAlert component

--- app/components/UIAlert.tsx
+++ app/components/UIAlert.tsx
@@ -2,10 +2,10 @@ import React, {Component, useState, useEffect} from 'react';
 import {StyleSheet, Modal, View, ModalProps} from 'react-native';
 
 // components
-import {UITextView} from '.';
+import {UIButton, UITextView} from '.';
 
 // constants
-import {COLORS} from '../constants';
+import {COLORS, DIMENSION} from '../constants';
 
 interface propTypes extends ModalProps {
   message: String;
@@ -15,8 +15,22 @@ const UIAlert = (props: propTypes) => {
   const [visible, setVisible] = useState<boolean>(true);
 
   return (
-    <Modal visible animationType="slide" transparent={true}>
-      <View style={styles.container}></View>
+    <Modal visible={visible} animationType="slide" transparent={true}>
+      <View style={styles.container}>
+        <View style={styles.messageContainer}>
+          <UITextView text={props.message} />
+
+          <UIButton
+            label="CLOSE"
+            width="100%"
+            buttonContainerStyle={{
+              paddingHorizontal: DIMENSION.MARGIN,
+              marginVertical: DIMENSION.MARGIN,
+            }}
+            onClick={() => setVisible(false)}
+          />
+        </View>
+      </View>
     </Modal>
   );
 };
@@ -26,6 +40,13 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0,0.7)',
     justifyContent: 'center',
     alignItems: 'center',
+    flex: 1,
+  },
+  messageContainer: {
+    backgroundColor: COLORS.white,
+    paddingHorizontal: DIMENSION.MARGIN * 2,
+    paddingVertical: DIMENSION.MARGIN,
+    borderRadius: DIMENSION.CARD_BORDER_RADIUS,
   },
 });
 
